5aa95ff2d300ebb9b7660390624134ae6d987511
psheng2 <psheng2@illinois.edu>
2020-06-04 18:28:17 -0500

    rm

--- bls12_relic.go
+++ bls12_relic.go
@@ -2,10 +2,6 @@
 
 package bls12
 
-// #include "relic_core.h"
-// #include "relic_ep.h"
-// void _fp_neg(fp_t r, const fp_t p) { fp_neg(r, p); }
-// void _bn_new(bn_t bn) { bn_new(bn); }
 import "C"
 
 var initDone = false
